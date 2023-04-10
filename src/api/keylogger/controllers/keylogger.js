abf41e312e96abe75d8676dec5fecffb47b87788
oybek <mukhiddinov.oybek1024@gmail.com>
2023-04-10 17:22:11 +0500

    agent api updated

--- src/api/keylogger/controllers/keylogger.js
+++ src/api/keylogger/controllers/keylogger.js
@@ -8,6 +8,7 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 
 const utils = require('../../../utils')
+const lodash = require("lodash");
 const { customError, uploader, parseJwt } = utils
 
 module.exports = createCoreController('api::keylogger.keylogger', ({strapi}) => ({
@@ -20,27 +21,35 @@ module.exports = createCoreController('api::keylogger.keylogger', ({strapi}) => 
     });
 
     if (!employee) return customError(ctx, 'Employee is not found. Check your token')
+    const _body = ctx.request.body
 
-    const { Icon, DateTime, Title, Process, Content } = ctx.request.body
+    if (!lodash.isArray(_body))return customError(ctx, 'Body is not array')
 
-    if (!Icon) return customError(ctx, 'Icon is required')
-    if (!DateTime) return customError(ctx, 'DateTime is required')
-    if (!Title) return customError(ctx, 'Title is required')
-    if (!Process) return customError(ctx, 'Process is required')
-    if (!Content) return customError(ctx, 'Content is required')
+    for await (const item of _body) {
+      const { Icon, DateTime, Title, Process, Content } = item
 
-    const _data = {
-      dateTime: DateTime,
-      employee: _token.id,
-      title: Title,
-      content: Content,
-      process: Process,
-      icon: await uploader(Icon)
+      if (!Icon) return customError(ctx, 'Icon is required')
+      if (!DateTime) return customError(ctx, 'DateTime is required')
+      if (!Title) return customError(ctx, 'Title is required')
+      if (!Process) return customError(ctx, 'Process is required')
+      if (!Content) return customError(ctx, 'Content is required')
+    }
+    for await (const item of _body) {
+      const { Icon, DateTime, Title, Process, Content } = item
+      const _data = {
+        dateTime: DateTime,
+        employee: _token.id,
+        title: Title,
+        content: Content,
+        process: Process,
+        icon: await uploader(Icon)
+      }
+
+      await strapi.entityService.create('api::keylogger.keylogger', {
+        data: _data
+      });
     }
 
-    await strapi.entityService.create('api::keylogger.keylogger', {
-      data: _data
-    });
     return {
       success: true,
       message: 'Keylogger created successfully',
--- src/api/webvisiting/controllers/webvisiting.js
+++ src/api/webvisiting/controllers/webvisiting.js
@@ -6,6 +6,7 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 const utils = require('../../../utils')
+const lodash = require("lodash");
 const { customError, parseJwt } = utils
 
 module.exports = createCoreController('api::webvisiting.webvisiting', ({strapi}) => ({
@@ -17,31 +18,35 @@ module.exports = createCoreController('api::webvisiting.webvisiting', ({strapi})
       populate: '*'
     });
 
+    const _body = ctx.request.body
     if (!employee) return customError(ctx, 'Employee is not found. Check your token')
 
-    const { Host, DateTime, Title, Page, Time, UserAgent } = ctx.request.body
-    //
-    if (!Host) return customError(ctx, 'Host is required')
-    if (!DateTime) return customError(ctx, 'DateTime is required')
-    if (!Title) return customError(ctx, 'Title is required')
-    if (!Page) return customError(ctx, 'Page is required')
-    if (!Time) return customError(ctx, 'Time is required')
-    //
-    const _data = {
-      dateTime: DateTime,
-      employee: employee.id,
-      title: Title,
-      browser: UserAgent,
-      duration: parseInt(Time),
-      host: Host,
-      page: Page
-    }
+    if (!lodash.isArray(_body)) return customError(ctx, 'Body is not array')
 
-    // return _data
-    //
-    await strapi.entityService.create('api::webvisiting.webvisiting', {
-      data: _data
-    });
+    for await (const item of _body) {
+      const { Host, DateTime, Title, Page, Time, UserAgent } = item
+
+      if (!Host) return customError(ctx, 'Host is required')
+      if (!DateTime) return customError(ctx, 'DateTime is required')
+      if (!Title) return customError(ctx, 'Title is required')
+      if (!Page) return customError(ctx, 'Page is required')
+      if (!Time) return customError(ctx, 'Time is required')
+    }
+    for await (const item of _body) {
+      const { Host, DateTime, Title, Page, Time, UserAgent } = item
+      const _data = {
+        dateTime: DateTime,
+        employee: employee.id,
+        title: Title,
+        browser: UserAgent,
+        duration: parseInt(Time),
+        host: Host,
+        page: Page
+      }
+      await strapi.entityService.create('api::webvisiting.webvisiting', {
+        data: _data
+      });
+    }
     return {
       success: true,
       message: 'Webvisiting created successfully',
